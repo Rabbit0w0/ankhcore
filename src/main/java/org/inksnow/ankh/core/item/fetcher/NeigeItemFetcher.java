c0933c76c643c5edf9397c718b0f23d460f6a4e1
InkerBot <im@inker.bot>
2023-04-08 13:53:57 +0800

    add bridge to NeigeItem

---
+++ src/main/java/org/inksnow/ankh/core/item/fetcher/NeigeItemFetcher.java
@@ -0,0 +1,29 @@
+package org.inksnow.ankh.core.item.fetcher;
+
+import lombok.val;
+import net.kyori.adventure.key.Key;
+import org.bukkit.inventory.ItemStack;
+import org.inksnow.ankh.core.api.item.ItemFetcher;
+import pers.neige.neigeitems.manager.ItemManager;
+
+import javax.annotation.Nonnull;
+import javax.inject.Singleton;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+@Singleton
+public class NeigeItemFetcher implements ItemFetcher {
+  @Override
+  public @Nonnull List<Key> fetchItem(@Nonnull ItemStack itemStack) {
+    val niItemInfo = ItemManager.INSTANCE.isNiItem(itemStack);
+    if (niItemInfo == null) {
+      return Collections.emptyList();
+    } else {
+      return Collections.singletonList(Key.key(
+          "NeigeItems".toLowerCase(Locale.ENGLISH),
+          niItemInfo.getId().toLowerCase(Locale.ENGLISH)
+      ));
+    }
+  }
+}
